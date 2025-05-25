5b8c09b5f8c71aa8cf86d793058c8098bd5e9ee8
hendras1722 <muhsyahendraa1722@gmail.com>
2025-05-25 16:16:21 +0700

    feat: upgrade package versions and refactor fetch logic
    - Updated package.json to version 2.0.1 and adjusted dependencies.
    - Refactored FetchData component to utilize new useFetch implementation from local composable.
    - Enhanced fetch functionality with abort capability and improved error handling.
    - Introduced createFetch utility for better API request management.
    - Added comprehensive type definitions and utility functions in usFetch.tsx.

--- src/app/v1/(dashboard)/fetch/page.tsx
+++ src/app/v1/(dashboard)/fetch/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { useFetch } from '@msa_cli/react-composable'
-import { FaSpinner } from 'react-icons/fa6'
+import { createFetch, useFetch } from '@/composable/usFetch'
+// import { createFetch, useFetch } from '@msa_cli/react-composable'
 
 interface User {
   id: number
@@ -16,50 +16,101 @@ export interface Jsonplaceholder {
   completed: boolean
 }
 export default function FetchData() {
-  const { data, error, isLoading } = useFetch<User>(
+  // Using useFetch to fetch data from an API
+  let AbortController: AbortController | null = null
+  const { data, error, abort, execute } = useFetch<User>(
     'https://jsonplaceholder.typicode.com/todos/1',
-    {},
     {
-      revalidateOnFocus: false,
-      errorRetryCount: 3,
+      afterFetch(ctx) {
+        ctx.data.title = 'Hunter x Hunter' // Modifies the response data
+        return ctx
+      },
+      beforeFetch: () => {
+        console.log('Fetching data...')
+      },
     }
   )
 
-  const { mutate } = useFetch(
-    'https://jsonplaceholder.typicode.com/posts',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title: 'foo',
-        body: 'bar',
-        userId: 1,
-      }),
-    },
-    {
-      responseType: 'json',
-      revalidateOnFocus: true,
-      errorRetryCount: 5,
-      onFetchError: (context) => {
-        console.error('Fetch failed:', context.error)
-        return {}
-      },
-    }
-  )
+  async function handleCancel() {
+    abort()
+  }
+
+  function getMyToken() {}
+
+  // If you want to control the behavior of beforeFetch, afterFetch, onFetchError between the pre-configured instance and newly spawned instance. You can provide a combination option to toggle between overwrite or chaining.
+  // const api = createFetch({
+  //   baseUrl: 'https://jsonplaceholder.typicode.com/',
+  //   combination: 'overwrite',
+  //   fetchOptions: { mode: 'cors' },
+  //   options: {
+  //     // beforeFetch in pre-configured instance will only run when the newly spawned instance do not pass beforeFetch
+  //     async beforeFetch({ options }) {
+  //       const myToken = await getMyToken()
+  //       // Ensure headers is an object before setting Authorization
+  //       if (!options.headers) {
+  //         options.headers = {}
+  //       }
+  //       if (options.headers instanceof Headers) {
+  //         options.headers.set('Authorization', `Bearer ${myToken}`)
+  //       } else if (Array.isArray(options.headers)) {
+  //         options.headers.push(['Authorization', `Bearer ${myToken}`])
+  //       } else {
+  //         ;(options.headers as Record<string, string>)[
+  //           'Authorization'
+  //         ] = `Bearer ${myToken}`
+  //       }
+
+  //       return { options }
+  //     },
+  //   },
+  // })
+  // const { post } = api<{
+  //   title: string
+  //   body: string
+  //   userId: number
+  // }>('/posts', {
+  //   immediate: false,
+  // })
+
+  const handleSubmit = async () => {
+    // method post, PUT, DELETE, PATCH set immediete to false
+    // if you want to use post, PUT, DELETE, PATCH with immediate: true, you can use the post method directly
+
+    execute()
+    // const res = await post<{
+    //   title: string
+    //   body: string
+    //   userId: number
+    // }>(
+    //   {
+    //     title: 'foo',
+    //     body: 'bar',
+    //     userId: 1,
+    //   },
+    //   'json'
+    // )
+
+    // console.log(res.data, 'inires')
+  }
 
   return (
     <div>
       <div className="mb-5 flex gap-3 items-center">
         <div>
           <div className="mb-2">useFetch update in version version 1.1</div>
-          <Button variant={'destructive'} onClick={mutate}>
+          <Button
+            variant={'destructive'}
+            onClick={handleSubmit}
+            className="mr-2"
+          >
             Fetch
+          </Button>
+          <Button variant={'destructive'} onClick={handleCancel}>
+            Cancel
           </Button>
         </div>
 
-        <div>{isLoading && <FaSpinner className="animate-spin" />}</div>
+        {/* <div>{isLoading && <FaSpinner className="animate-spin" />}</div> */}
       </div>
 
       <div>{error && 'Error Fetching'}</div>
@@ -68,83 +119,72 @@ export default function FetchData() {
 
       <pre className="bg-gray-100 rounded-lg overflow-auto h-96 mt-3  p-3">
         <code className="text-black">{`
-'use client'
+// method post, PUT, DELETE, PATCH set immediete to false
+// if you want to use post, PUT, DELETE, PATCH with immediate: true, you can use the post method directly
 
-import { Button } from '@/components/ui/button'
-import { useFetch } from '@msa_cli/react-composable'
-import { FaSpinner } from 'react-icons/fa6'
+const { data, error } = useFetch<User>(
+  'https://jsonplaceholder.typicode.com/todos/1',
+    {
+      immediate: false, // set immediate to false to prevent automatic fetching
+      afterFetch(ctx) {
+        ctx.data.title = 'Hunter x Hunter' // Modifies the response data
+        return ctx
+      },
+      beforeFetch: () => {
+        console.log('Fetching data...')
+      },
+    }
+)
 
-interface User {
-  id: number
-  name: string
-  email: string
-}
-export interface Jsonplaceholder {
-  userId: number
-  id: number
+
+const res = await post<{
   title: string
-  completed: boolean
-}
-export default function FetchData() {
-  const { data, error, isLoading } = useFetch<User>(
-    'https://jsonplaceholder.typicode.com/todos/1',
-    {},
-    {
-      revalidateOnFocus: false,
-      errorRetryCount: 3,
-    }
-  )
+  body: string
+  userId: number
+}>(
+  {
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  },
+  'json'
+)
 
-  const {
-    data: DataPost,
-    isLoading: loadingPost,
-    mutate,
-  } = useFetch(
-    'https://jsonplaceholder.typicode.com/posts',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+
+ // If you want to control the behavior of beforeFetch, afterFetch, onFetchError between the pre-configured instance and newly spawned instance. You can provide a combination option to toggle between overwrite or chaining.
+  const api = createFetch({
+    baseUrl: 'https://jsonplaceholder.typicode.com/',
+    combination: 'overwrite',
+    fetchOptions: { mode: 'cors' },
+    options: {
+      // beforeFetch in pre-configured instance will only run when the newly spawned instance do not pass beforeFetch
+      async beforeFetch({ options }) {
+        const myToken = await getMyToken()
+        // Ensure headers is an object before setting Authorization
+        if (!options.headers) {
+          options.headers = {}
+        }
+        if (options.headers instanceof Headers) {
+          options.headers.set('Authorization', Bearer myToken)
+        } else if (Array.isArray(options.headers)) {
+          options.headers.push(['Authorization', Bearer myToken])
+        } else {
+          ;(options.headers as Record<string, string>)[
+            'Authorization'
+          ] = 'Bearer myToken'
+        }
+
+        return { options }
       },
-      body: JSON.stringify({
-        title: 'foo',
-        body: 'bar',
-        userId: 1,
-      }),
     },
-    {
-      responseType: 'json',
-      revalidateOnFocus: true,
-      errorRetryCount: 5,
-      onFetchError: (context) => {
-        console.error('Fetch failed:', context.error)
-        return {}
-      },
-    }
-  )
-
-  function handleCLick() {
-    mutate()
-  }
-  return (
-    <div>
-      <div className="mb-5 flex gap-3 items-center">
-        <Button variant={'destructive'} onClick={mutate}>
-          Fetch
-        </Button>
-        {/*
-        // */}
-        <div>{isLoading && <FaSpinner className="animate-spin" />}</div>
-      </div>
-
-      <div>{error && 'Error Fetching'}</div>
-      <div>{data && JSON.stringify(data, null, 2)}</div>
-      <div className="mt-5">How to use:</div>
-      
-      
-    </div>
-  )
-}
+  })
+  const { post } = api<{
+    title: string
+    body: string
+    userId: number
+  }>("/posts", {
+    immediate: false,
+  })
       `}</code>
       </pre>
     </div>
